3b0ff8f0131508a6798865a5092efbea3f3db071
chary <920868587@qq.com>
2024-04-21 14:29:40 +0800

    1

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -4,3 +4,7 @@ export async function GET(req:Response) {
 
     return new Response(JSON.stringify({body:req.body,url:req.url}))
 }
+export async function POST(req:Response) {
+
+    return new Response(JSON.stringify({body:req.body,url:req.url}))
+}
