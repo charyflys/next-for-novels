42b893775bf4f4cba64e5d0e2e9af1307d419f15
chary <920868587@qq.com>
2024-04-21 13:28:40 +0800

    对照

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -1,5 +1,5 @@
 import redis from "../../lib/redis";
 
 export function GET(req:Request,res: Response) {
-    return new Response(JSON.stringify('query' in req&&req.query as any))
+    return new Response(JSON.stringify(req))
 }
