f7f203a16124d140d977dab7b72122572bfde4c3
chary <920868587@qq.com>
2024-04-21 18:56:17 +0800

    修改依赖，使用querystring解析

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -1,13 +1,13 @@
 import redis from "../../lib/redis";
-import url from 'url'
+import querystring from 'node:querystring'
 export const runtime = "edge";
 export async function GET(req:Request) {
-    const qs = url.parse(req.url,true)
+    const qs = querystring.parse(req.url)
     // const key = qs.redis.get(qs.key)
     return Response.json(qs)
 }
 export async function POST(req:Request) {
     const body = await req.text()
-    const qs = url.parse(body)
+    const qs = querystring.parse(body)
     return Response.json(qs)
 }
