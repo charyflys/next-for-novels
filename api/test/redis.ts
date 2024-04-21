ad0a94e1f09419066d73cb48ed91436453e899d3
chary <920868587@qq.com>
2024-04-21 13:33:14 +0800

    再试试

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -1,5 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
 import redis from "../../lib/redis";
 
-export function GET(req:Request,res: Response) {
-    return new Response(JSON.stringify(req))
+export async function GET(req:NextRequest) {
+    const value = req.nextUrl.searchParams.get('key')
+    const query = req.nextUrl.searchParams.getAll('text')
+    return NextResponse.json({
+        value,
+        query
+    })
 }
