f5b0f662271c66037311c5c63369897651924441
chary <920868587@qq.com>
2024-04-21 13:57:37 +0800

    值测试

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -1,10 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
 // import redis from "../../lib/redis";
 
-export async function GET(req:NextRequest) {
-    const arr = []
-    for(const key in req) {
-        arr.push(key)
-    }
-    return new Response(JSON.stringify(arr))
+export async function GET(req:Response) {
+
+    return new Response(JSON.stringify({body:req.body,url:req.url}))
 }
