27847e151a7d014ce6082354bf11ad5e5351daa5
chary <920868587@qq.com>
2024-04-21 22:55:10 +0800

    针对get进行修改，并修改post的识别方式

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -2,12 +2,15 @@ import redis from "../../lib/redis";
 import querystring from 'node:querystring'
 export const runtime = "edge";
 export async function GET(req:Request) {
-    const qs = querystring.parse(req.url)
+    const qs = querystring.parse(req.url.replace(/^.+?\?/,''))
     // const key = qs.redis.get(qs.key)
     return Response.json(qs)
 }
 export async function POST(req:Request) {
     const body = await req.text()
-    const qs = querystring.parse(body)
+    const ContentType = req.headers.get('Content-Type') 
+    const qs = ContentType === "application/json" ?
+    JSON.parse(body):
+    querystring.parse(body)
     return Response.json(qs)
 }
