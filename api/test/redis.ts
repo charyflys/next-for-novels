4b77e58121c8da4bac9a88c0cd7dd41062aa3f3d
chary <920868587@qq.com>
2024-04-21 14:54:05 +0800

    看看是啥

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -4,11 +4,13 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 export async function GET(req:Request) {
     const req1 = new NextRequest(req)
+    req1.nextUrl.searchParams
     return NextResponse.json({
         body:req1.body,
         url:req1.url,
         ip: req1.ip,
-        key: req1.nextUrl.searchParams.get('key')
+        key: req1.nextUrl.searchParams.get('key'),
+        query: req1.nextUrl.searchParams
     })
 }
 export async function POST(req:Request) {
