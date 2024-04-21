612883f31cbfd1f37bcf7e1a49f0d387eb93e780
chary <920868587@qq.com>
2024-04-21 12:59:39 +0800

    解决完毕，正式测试

--- api/test/redis.ts
+++ api/test/redis.ts
@@ -1,5 +1,5 @@
 import redis from "../../lib/redis";
 
 export default function get(req:Request) {
-    return new Response(JSON.stringify(req))
+    return new Response('query' in req&&req.query as any)
 }
