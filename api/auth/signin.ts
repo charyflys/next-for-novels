cb1c004f38bcb625b9556d86fec2578e456f560d
chary <920868587@qq.com>
2024-04-23 21:29:04 +0800

    修改了部分cookies设置细节

--- api/auth/signin.ts
+++ api/auth/signin.ts
@@ -12,19 +12,20 @@ export async function POST(req:Request) {
     if (error) {
         return resultNoData(error.message, '500')
     }
+    const time = Date.now()
     const res = result(data.user).headers
-    .set('Set-Cookie',`_Host-token=${generateJWT(data.session)};Secure;`)
+    .set('Set-Cookie',`_Secure-token="${generateJWT(data.session)}"; Secure; Max-Age=604800000`)
     return res
 }
 
 export async function GET(req:Request) {
     const nextreq = new NextRequest(req)
-    const token = nextreq.cookies.get('_Host-token')
+    const token = nextreq.cookies.get('_Secure-token')
     if (token){
         const Session = explainJWT(token.value) as Session
         const { data } = await supabase.auth.setSession(Session)
         const res = result(data.user).headers
-        .set('Set-Cookie',`_Host-token=${generateJWT(data.session)};Secure;`)
+        .set('Set-Cookie',`_Secure-token="${generateJWT(data.session)}"; Secure; Max-Age=604800000`)
         return res
     }
     return resultNoData('您未登录，请先登录','403')
