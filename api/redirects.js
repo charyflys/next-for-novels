afdb2b7938ba9183ac16b9667490757fd8c68ebc
chary <920868587@qq.com>
2024-04-23 23:41:21 +0800

    设置rule试试

--- api/redirects.js
+++ api/redirects.js
@@ -1,8 +1,8 @@
 export default function Redirect (req, res) {
     // Insert redirect rules here
-    if (req.url.startsWith('/example')) {
+    if (req.url.startsWith('/novel')) {
       res.statusCode = 308
-      res.setHeader('location', '/test')
+      res.setHeader('location', '/articleview')
       // Caching headers
       res.set('Cache-control', 's-maxage=600')
       return res.end()
