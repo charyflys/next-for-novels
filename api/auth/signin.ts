8f32d313e15e15bc814c0725614bafc4236e04f3
chary <920868587@qq.com>
2024-04-23 21:44:03 +0800

    找到问题了

--- api/auth/signin.ts
+++ api/auth/signin.ts
@@ -14,7 +14,7 @@ export async function POST(req:Request) {
     }
     const time = Date.now()
     const res = result(data.user)
-    res.headers.set('Set-Cookie',`_Secure-token="${generateJWT(data.session)}"; Secure; Max-Age=604800000`)
+    res.headers.set('Set-Cookie',`_Secure-token=${generateJWT(data.session)}; Secure; Max-Age=604800000`)
     
     return res
 }
@@ -23,12 +23,11 @@ export async function GET(req:Request) {
     const nextreq = new NextRequest(req)
     const token = nextreq.cookies.get('_Secure-token')
     if (token){
-        return result(token.value)
-        // const Session = explainJWT(token.value) as Session
-        // const { data } = await supabase.auth.setSession(Session)
-        // const res = result(data.user)
-        // res.headers.set('Set-Cookie',`_Secure-token="${generateJWT(data.session)}"; Secure; Max-Age=604800000`)
-        // return res
+        const Session = explainJWT(token.value) as Session
+        const { data } = await supabase.auth.setSession(Session)
+        const res = result(data.user)
+        res.headers.set('Set-Cookie',`_Secure-token="${generateJWT(data.session)}"; Secure; Max-Age=604800000`)
+        return res
     }
     return resultNoData('您未登录，请先登录','403')
 }
