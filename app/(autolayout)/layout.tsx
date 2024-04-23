33392fda9b819b3f2a38bdb2c59742fadc3b80cd
charyflys <167504273+charyflys@users.noreply.github.com>
2024-04-23 13:42:52 +0800

    更新 layout.tsx,查看服务端组件到底返回了什么

--- app/(autolayout)/layout.tsx
+++ app/(autolayout)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
     supabase.auth.getSession()
-    .then(session => console.log(session.data.session))
+    .then(session => console.log(session))
     .catch(err => console.error(err))
     return (
       <AllLayout props={{showcopyright:false}}>
