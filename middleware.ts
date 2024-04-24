7c499860db84bd4839dabe62cbd88c6ea0b921a5
chary <920868587@qq.com>
2024-04-24 20:16:21 +0800

    我是傻逼2

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { hostTokenName } from './lib/env-values'
+import { explainJWT } from './lib/quickapi';
+import supabase from './lib/supabaseClient';
+import { Session } from '@supabase/supabase-js';
+export default async function middleware(request: NextRequest) {
+  // Extract country. Default to US if not found.
+    // const req = new NextRequest(request)
+    const cookie = request.cookies.get(hostTokenName)
+    if (cookie) {
+        const session = explainJWT(cookie.value) as Session
+        const { data,error } = await supabase.auth.setSession(session)
+        if (error||(!data.session)) {
+            return NextResponse.redirect('/signin')
+        }
+    }
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/novel',
+        '/novels',
+        '/favorite',
+        '/search',
+        '/announce',
+    ],
+};
