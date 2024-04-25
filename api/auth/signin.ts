583b2bf1ed0678dcabf94a72cb558b0a541924d3
chary <920868587@qq.com>
2024-04-25 21:30:34 +0800

    再看看

--- api/auth/signin.ts
+++ api/auth/signin.ts
@@ -7,36 +7,40 @@ import redis from "../../lib/redis";
 import md5 from 'md5'
 import Refresh from "../../lib/supabase/refreshtoken";
 
-export async function POST(req:Request) {
-    
+export async function POST(req: Request) {
+
     const { email, password } = await getBody<{ email: string, password: string }>(req)
     const { error, data } = await supabase.auth.signInWithPassword({
-        email,password
+        email, password
     })
     if (error) {
         return resultNoData(error.message, '500')
     }
-    const { access_token,refresh_token } = data.session
-    const session = { access_token,refresh_token,time: Date.now() } 
+    const { access_token, refresh_token } = data.session
+    const session = { access_token, refresh_token, time: Date.now() }
     const jwt = await generateJWT(session)
-    const redisresult= await redis.set(md5(jwt),data.user,
-        // {px: 60}
+    const redisresult = await redis.set(md5(jwt), data.user,
+        { px: 600 }
     )
-    const res = result({user:data.user,redisresult,md5:md5(jwt)})
-    res.headers.set('Set-Cookie',`${hostTokenName}=${jwt} ; Path= /; Max-Age=2592000; Secure `)
-    
+    const res = result({ 
+        user: data.user,
+        // redisresult,
+        // md5: md5(jwt)
+    })
+    res.headers.set('Set-Cookie', `${hostTokenName}=${jwt} ; Path= /; Max-Age=2592000; Secure `)
+
     return res
 }
 
-export async function GET(req:Request) {
+export async function GET(req: Request) {
     const nextreq = new NextRequest(req)
     const token = nextreq.cookies.get(hostTokenName)
-    if (token){
+    if (token) {
         const Session = (await explainJWT<Session>(token.value))
         const { data } = await supabase.auth.setSession(Session)
-        if (!data.user)return resultNoData('登陆失效','403')
+        if (!data.user) return resultNoData('登陆失效', '403')
         const res = result(data.user)
-        return Refresh(res,data.session,Session,data.user)
+        return Refresh(res, data.session, Session, data.user)
     }
-    return resultNoData('您未登录，请先登录','403')
+    return resultNoData('您未登录，请先登录', '403')
 }
