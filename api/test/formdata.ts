4253e1197cc6ecbd8fc98dc2880c6fd52dc0a0e9
chary <920868587@qq.com>
2024-05-22 00:02:35 +0800

    formdata怎么获取呢

--- api/test/formdata.ts
+++ api/test/formdata.ts
@@ -1,6 +1,18 @@
 import { getBody, result } from "../../lib/quickapi";
 
+
+
 export async function POST(req:Request) {
-    const obj = await req.text()
-    return result(200, obj)
+    const obj = await req.formData()
+    const res = {} as any
+    obj.forEach((item,key) => {
+        res[key] = item
+        if (item instanceof Blob) {
+            res[key] = item.size
+        }
+    })  
+    // const form = new multiparty.Form();
+    // form.parse(req)
+    // const obj = await req.text()
+    return result(200, res)
 }
