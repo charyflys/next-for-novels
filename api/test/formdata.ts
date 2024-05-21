0f0dee955b64edbf2321e006c8f56ca488e37bef
chary <920868587@qq.com>
2024-05-21 23:49:58 +0800

    看看body

--- api/test/formdata.ts
+++ api/test/formdata.ts
@@ -1,6 +1,6 @@
 import { getBody, result } from "../../lib/quickapi";
 
 export async function POST(req:Request) {
-    const obj = await getBody(req) as any
+    const obj = await req.text()
     return result(200, obj)
 }
