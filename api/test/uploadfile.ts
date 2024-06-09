7e605676dd0f80e66d41951a4715a1aa3814419b
chary <920868587@qq.com>
2024-06-09 15:52:09 +0800

    看是否能把文件取回来

--- api/test/uploadfile.ts
+++ api/test/uploadfile.ts
@@ -1,4 +1,4 @@
-import { getBody, result } from "../../lib/quickapi";
+import { resultNoData, result, getQuery } from "../../lib/quickapi";
 import { addArticle, getArticle } from "../../lib/supabase/articleBlob";
 
 
@@ -17,12 +17,21 @@ export async function POST(req:Request) {
         }
     })  
     if (!file) {
-        return result(200, res)
+        return result(res)
     }
     const uploadFile = new File([file],'/test/article.txt')
     const re = await addArticle(uploadFile)
     if (re.err) {
-        return result(403,re.msg)
+        return resultNoData(re.msg,'403')
     }
-    return result(200,re.path)
+    return resultNoData(re.path)
+}
+
+export async function GET(req:Request) {
+    const { name } = await getQuery(req)
+    const re = await getArticle(name as string)
+    if (re.err) {
+        return resultNoData(re.msg,'403')
+    }
+    return new Response(re.file,{status: 200,})
 }
