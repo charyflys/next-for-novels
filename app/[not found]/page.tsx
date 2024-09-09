15e207a06690d1923380dab21d2b3520aa90270a
chary <920868587@qq.com>
2024-09-09 20:51:49 +0800

    处理404页面

---
+++ app/[not found]/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Page() {
+    return (
+        <div className="container-404">
+            <div className="copy-container center-xy">
+                <p>
+                    404, page not found.<br/>
+                    Press any key to continue......
+                </p>
+                <span className="handle"></span>
+
+            </div>
+        </div>
+    )
+}
